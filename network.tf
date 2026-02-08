daafbef5e4a1d887031813a6af26950880d3da84
Hideaki-Anzai <anzai1995.13@gmail.com>
2026-02-08 23:22:57 +0900

    Update network.tf

--- network.tf
+++ network.tf
@@ -14,3 +14,51 @@ resource "aws_vpc" "vpc" {
     environment = var.environment
   }
 }
+
+#------------------------
+#Route Table        
+#------------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-public-rt"
+    project     = var.project
+    environment = var.environment
+    type        = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_1a" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+
+}
+
+resource "aws_route_table_association" "public_rt_1c" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-private-rt"
+    project     = var.project
+    environment = var.environment
+    type        = "private"
+  }
+}
+resource "aws_route_table_association" "private_rt_1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+
+}
+
+resource "aws_route_table_association" "private_rt_1c" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
+
+}
